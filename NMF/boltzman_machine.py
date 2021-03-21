b8b0ede83fdac5c4e2c92eb42aedc20893522869
Rayane Ait Ali Yahia <rayane.ait_ali_yahia@g.enp.edu.dz>
2021-03-21 19:34:16 +0100

    bloztman update class

--- NMF/boltzman_machine.py
+++ NMF/boltzman_machine.py
@@ -1,34 +1,33 @@
-
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as I
+
+
+
 class Net(nn.Module):
 
     def __init__(self,
-                 input,
-                 output):
+                 input_layer,
+                 output_layer):
         super(Net, self).__init__()
-        self.input = input
-        self.output = output
+        self.input_layer = input_layer
+        self.output_layer = output_layer
         self.sigmoid = torch.nn.Sigmoid()
         
-        self.feed = nn.Linear(input, output)  
+        self.feed = nn.Linear(input_layer, output_layer)  
 
 
     def forward(self, x):
         h = self.sigmoid(self.feed(x))        # x -> h 
 
-        x_reconstructed = torch.matmul(self.feed.weight.t(), h.t())
+        x_reconstructed = torch.matmul(h,self.feed.weight)
         
-        h_reconstructed = self.sigmoid(self.feed(x_reconstructed.t()))# h-> x'
+        h_reconstructed = self.sigmoid(self.feed(x_reconstructed))# h-> x'
 
-
-
-
-        return x_reconstructed, h , h_reconstructed
+        return h,x_reconstructed,h_reconstructed
 
 data = torch.rand(5210,257)
-model = Net(input = data.shape[1],output = 200)
+model = Net(data.shape[1], 200)
 model(data)
