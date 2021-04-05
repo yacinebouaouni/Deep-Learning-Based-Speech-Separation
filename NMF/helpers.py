cac1b2c68d7e81fb53fee73da06433ebc08976a9
yacinebouaouni07@gmail.com <yacinebouaouni07@gmail.com>
2021-04-05 17:38:31 +0200

    Correct SDR

--- NMF/helpers.py
+++ NMF/helpers.py
@@ -90,9 +90,8 @@ def SDR(s_est, s):
     """
     
     signal_power = LA.norm(s,2)
-    signal_est_power = LA.norm(s_est,2)
-    
-    SDR_db=10*np.log10(signal_power**2/((signal_est_power-signal_power)**2))
+    distorsion_power = LA.norm(s_est - s,2) 
+    SDR_db=10*np.log10(signal_power/distorsion_power)
     
     return SDR_db
 
