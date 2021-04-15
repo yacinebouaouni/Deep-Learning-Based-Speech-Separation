591cd575916c66911ea83213255ee0a06510a520
yacinebouaouni07@gmail.com <yacinebouaouni07@gmail.com>
2021-04-15 15:58:36 +0200

    Gpu-yacine

--- Model/Model.py
+++ Model/Model.py
@@ -29,15 +29,15 @@ class Net(nn.Module):
     
     
     
-class Net_r(nn.Module):
+class Netsmall(nn.Module):
 
     def __init__(self,d):
-        super(Net, self).__init__()
+        super(Netsmall, self).__init__()
         self.sigmoid = torch.nn.Sigmoid()
         self.fc1 = nn.Linear(d, 100)  # d is dimension of the input.
         self.fc2 = nn.Linear(100, 50)
-        self.fc3 = nn.Linear(50, 200)
-        self.fc4 = nn.Linear(200,2)
+        self.fc3 = nn.Linear(50, 10)
+        self.fc4 = nn.Linear(10,2)
         
 
     def forward(self, x):
